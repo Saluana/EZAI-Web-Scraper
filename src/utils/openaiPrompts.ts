5f0708ce0622285f4409470c5e710330b59ef26a
Saluana <46250649+Saluana@users.noreply.github.com>
2023-01-10 17:07:27 -0800

    update

--- src/utils/openaiPrompts.ts
+++ src/utils/openaiPrompts.ts
@@ -17,13 +17,13 @@ async function getNote(chunk: string, apiKey: string): Promise<string | null> {
     try {
         var response = await openai.createCompletion('text-curie-001', {
             prompt: `
-        create a bullet point list of short notes from key topics in the provided text using "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.\n\n
-        example text: Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.\n\n
-        example notes: ->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.\n\n
-        example text: Pitbulls are muscular dogs with strong jaws. They were originally bred for bull-baiting, but are now used as guard dogs or for dog fighting. Despite their reputation, they can be loving pets with proper training and responsible ownership.\n\n
-        example notes: ->Pitbulls are a type of dog known for their muscular build and strong jaws. ->Pitbulls were originally bred for bull-baiting. ->Pitbulls are now used as guard dogs or for dog fighting. ->Pitbulls can be loving pets with proper training and responsible ownership.\n\n
-        Text (Do not include any information from example notes, or  example text. Use as a guide only.):${chunk}\n\n
-        Notes: ->`,
+            create a bullet point list of short notes from key topics in the provided text using "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.\n\n
+            example text: Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.\n\n
+            example notes: ->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.\n\n
+            example text: Pitbulls are muscular dogs with strong jaws. They were originally bred for bull-baiting, but are now used as guard dogs or for dog fighting. Despite their reputation, they can be loving pets with proper training and responsible ownership.\n\n
+            example notes: ->Pitbulls are a type of dog known for their muscular build and strong jaws. ->Pitbulls were originally bred for bull-baiting. ->Pitbulls are now used as guard dogs or for dog fighting. ->Pitbulls can be loving pets with proper training and responsible ownership.\n\n
+            Text (Do not include any information from example notes, or  example text. Use as a guide only.): ${chunk}\n\n
+            Notes: ->`,
             temperature: 0.9,
             max_tokens: 850,
             top_p: 1.0,
