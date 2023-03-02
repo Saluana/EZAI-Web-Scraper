a010ed65e0cfe8d1d3607300d6b9f4966a576966
Saluana <46250649+Saluana@users.noreply.github.com>
2023-03-01 18:08:47 -0800

    Upgraded to GPT-3.5-turbo

--- dist/src/utils/openaiPrompts.js
+++ dist/src/utils/openaiPrompts.js
@@ -12,42 +12,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.openai = void 0;
 const { Configuration, OpenAIApi } = require('openai');
 const contentFilter_1 = __importDefault(require("./contentFilter"));
-function configureAI(key) {
-    const configuration = new Configuration({
-        apiKey: key,
-    });
-    const openai = new OpenAIApi(configuration);
-    return openai;
-}
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+exports.openai = new OpenAIApi(configuration);
 /*    NOTE SECTION   */
 function getNote(chunk, apiKey) {
     return __awaiter(this, void 0, void 0, function* () {
-        const openai = configureAI(apiKey);
+        let response;
         try {
-            var response = yield openai.createCompletion('text-curie-001', {
-                prompt: `
-            create a bullet point list of short notes from key topics in the provided text using "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.\n\n
-            example text: Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.\n\n
-            example notes: ->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.\n\n
-            example text: Pitbulls are muscular dogs with strong jaws. They were originally bred for bull-baiting, but are now used as guard dogs or for dog fighting. Despite their reputation, they can be loving pets with proper training and responsible ownership.\n\n
-            example notes: ->Pitbulls are a type of dog known for their muscular build and strong jaws. ->Pitbulls were originally bred for bull-baiting. ->Pitbulls are now used as guard dogs or for dog fighting. ->Pitbulls can be loving pets with proper training and responsible ownership.\n\n
-            Text (Do not include any information from example notes, or  example text. Use as a guide only.): ${chunk}\n\n
-            Notes: ->`,
-                temperature: 0.9,
-                max_tokens: 850,
-                top_p: 1.0,
-                frequency_penalty: 0.5,
-                presence_penalty: 0.5,
+            response = yield exports.openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        //MARKDOWN
+                        role: 'system',
+                        content: 'I need you to create a bullet point list of short notes from key topics in the text the user has provided. Use "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.',
+                    },
+                    {
+                        //RULES
+                        role: 'user',
+                        content: 'Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.',
+                    },
+                    {
+                        //RULES
+                        role: 'assistant',
+                        content: '->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.',
+                    },
+                    {
+                        //RULES
+                        role: 'user',
+                        content: chunk,
+                    },
+                ],
+                temperature: 0.85,
             });
-            console.log(response.data.choices[0]);
+            console.log(response.data.choices[0].message.content);
         }
         catch (error) {
             console.log(error);
             return null;
         }
-        return response.data.choices[0].text;
+        return response.data.choices[0].message.content;
     });
 }
 function getNotes(splitContent, apiKey) {
@@ -82,26 +91,30 @@ function getNotes(splitContent, apiKey) {
 /*      SUMMARY SECTION      */
 function getSummaryChunk(chunk, apiKey) {
     return __awaiter(this, void 0, void 0, function* () {
-        const openai = configureAI(apiKey);
+        let response;
         try {
-            var response = yield openai.createCompletion('text-curie-001', {
-                prompt: `
-            Write a concise summary of the following article, and remove any unwanted text, such as things related to website cookies,  website newletters, and website advertisements.
-            Article:${chunk}
-            Summary:`,
-                temperature: 0.9,
-                max_tokens: 700,
-                top_p: 1.0,
-                frequency_penalty: 0.5,
-                presence_penalty: 0.5,
+            response = yield exports.openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        //MARKDOWN
+                        role: 'system',
+                        content: 'Write a short and concise summary of the text provided by the user. Remove any unwanted text, such as things related to website cookies, website newletters, and website advertisements.',
+                    },
+                    {
+                        //RULES
+                        role: 'user',
+                        content: chunk,
+                    },
+                ],
+                temperature: 0.85,
             });
-            console.log(response.data.choices[0]);
         }
         catch (error) {
             console.log(error);
             return null;
         }
-        const summarizedText = response.data.choices[0].text;
+        const summarizedText = response.data.choices[0].message.content;
         return summarizedText;
     });
 }
--- src/utils/openaiPrompts.ts
+++ src/utils/openaiPrompts.ts
@@ -2,42 +2,54 @@ const { Configuration, OpenAIApi } = require('openai');
 import contentFilter from './contentFilter';
 import { SuccessMessage, ErrorMessage } from '../types/types';
 
-function configureAI(key: string) {
-    const configuration = new Configuration({
-        apiKey: key,
-    });
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-    const openai = new OpenAIApi(configuration);
-    return openai;
-}
+export const openai = new OpenAIApi(configuration);
 
 /*    NOTE SECTION   */
 async function getNote(chunk: string, apiKey: string): Promise<string | null> {
-    const openai = configureAI(apiKey);
+    let response;
+
     try {
-        var response = await openai.createCompletion('text-curie-001', {
-            prompt: `
-            create a bullet point list of short notes from key topics in the provided text using "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.\n\n
-            example text: Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.\n\n
-            example notes: ->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.\n\n
-            example text: Pitbulls are muscular dogs with strong jaws. They were originally bred for bull-baiting, but are now used as guard dogs or for dog fighting. Despite their reputation, they can be loving pets with proper training and responsible ownership.\n\n
-            example notes: ->Pitbulls are a type of dog known for their muscular build and strong jaws. ->Pitbulls were originally bred for bull-baiting. ->Pitbulls are now used as guard dogs or for dog fighting. ->Pitbulls can be loving pets with proper training and responsible ownership.\n\n
-            Text (Do not include any information from example notes, or  example text. Use as a guide only.): ${chunk}\n\n
-            Notes: ->`,
-            temperature: 0.9,
-            max_tokens: 850,
-            top_p: 1.0,
-            frequency_penalty: 0.5,
-            presence_penalty: 0.5,
+        response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    //MARKDOWN
+                    role: 'system',
+                    content:
+                        'I need you to create a bullet point list of short notes from key topics in the text the user has provided. Use "->" as the bullet point for each note. Please remove and ignore any unwanted text, such as things related to website cookies,  website newletters, and website advertisements. Use the example text, and example notes as a guide.',
+                },
+                {
+                    //RULES
+                    role: 'user',
+                    content:
+                        'Penguins are flightless birds that live in cold climates, primarily in Antarctica. They have adapted to their environment by developing thick feathers and a layer of fat, called blubber, to keep warm in the freezing water.',
+                },
+                {
+                    //RULES
+                    role: 'assistant',
+                    content:
+                        '->Penguins are flightless birds. ->Live in cold climates, primarily in Antarctica. ->Have adapted to their environment with thick feathers and blubber. ->Blubber helps them stay warm in freezing water.',
+                },
+                {
+                    //RULES
+                    role: 'user',
+                    content: chunk,
+                },
+            ],
+            temperature: 0.85,
         });
 
-        console.log(response.data.choices[0]);
+        console.log(response.data.choices[0].message.content);
     } catch (error) {
         console.log(error);
         return null;
     }
 
-    return response.data.choices[0].text;
+    return response.data.choices[0].message.content;
 }
 
 async function getNotes(
@@ -81,27 +93,30 @@ async function getSummaryChunk(
     chunk: string,
     apiKey: string
 ): Promise<string | null> {
-    const openai = configureAI(apiKey);
-
+    let response;
     try {
-        var response = await openai.createCompletion('text-curie-001', {
-            prompt: `
-            Write a concise summary of the following article, and remove any unwanted text, such as things related to website cookies,  website newletters, and website advertisements.
-            Article:${chunk}
-            Summary:`,
-            temperature: 0.9,
-            max_tokens: 700,
-            top_p: 1.0,
-            frequency_penalty: 0.5,
-            presence_penalty: 0.5,
+        response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    //MARKDOWN
+                    role: 'system',
+                    content:
+                        'Write a short and concise summary of the text provided by the user. Remove any unwanted text, such as things related to website cookies, website newletters, and website advertisements.',
+                },
+                {
+                    //RULES
+                    role: 'user',
+                    content: chunk,
+                },
+            ],
+            temperature: 0.85,
         });
-
-        console.log(response.data.choices[0]);
     } catch (error) {
         console.log(error);
         return null;
     }
-    const summarizedText: string = response.data.choices[0].text;
+    const summarizedText: string = response.data.choices[0].message.content;
     return summarizedText;
 }
 
